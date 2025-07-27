f1fd2437212a4bdc90f651c6ed424bbb13a46a89
Ahttpe <ahttpe@MBP-de-Rafael.home>
2025-07-27 12:08:24 -0300

    corrigido um bug da criação do sharedpreferences quando o arquivo fica corrompido. adicionei um tratamento para deletar e recria-lo caso isso aconteca

--- core/src/main/java/br/com/desafiomercantil/core/di/providers/Providers.kt
+++ core/src/main/java/br/com/desafiomercantil/core/di/providers/Providers.kt
@@ -8,36 +8,47 @@ import androidx.security.crypto.EncryptedSharedPreferences
 import java.security.KeyStore
 import javax.crypto.KeyGenerator
 
+private const val FILE_NAME = "secure_prefs"
+private val KEY_ALIAS = "secure_key"
+
 fun provideEncryptedSharedPreferences(context: Context): SharedPreferences {
-    val keyAlias = "secure_key"
-    val fileName = "secure_prefs"
+    return try {
+        val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
+            load(null)
+        }
 
-    val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
-        load(null)
-    }
+        if (!keyStore.containsAlias(KEY_ALIAS)) {
+            val keyGenerator =
+                KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+                KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .setUserAuthenticationRequired(false)
+                .build()
 
-    if (!keyStore.containsAlias(keyAlias)) {
-        val keyGenerator =
-            KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
-        val keyGenParameterSpec = KeyGenParameterSpec.Builder(
-            keyAlias,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            keyGenerator.init(keyGenParameterSpec)
+            keyGenerator.generateKey()
+        }
+
+        return EncryptedSharedPreferences.create(
+            FILE_NAME,
+            KEY_ALIAS,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
-            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-            .setUserAuthenticationRequired(false)
-            .build()
-
-        keyGenerator.init(keyGenParameterSpec)
-        keyGenerator.generateKey()
+    } catch (e: Exception) {
+        context.deleteSharedPreferences(FILE_NAME)
+        EncryptedSharedPreferences.create(
+            FILE_NAME,
+            KEY_ALIAS,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
-
-    return EncryptedSharedPreferences.create(
-        fileName,
-        keyAlias,
-        context,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
 }
 
